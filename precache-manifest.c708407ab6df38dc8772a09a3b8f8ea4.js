d9fea669b07e5e61e8f424d25feb3bcd42f21ea3
abdellani <abdellani@gmail.com>
2019-11-24 16:53:18 +0100

    Updates

---
+++ precache-manifest.c708407ab6df38dc8772a09a3b8f8ea4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304a69ec5de3bf65a2dbc89e5f91b457",
+    "url": "/ONLINE_STORE_FE/index.html"
+  },
+  {
+    "revision": "d41e08de5520826bd24f",
+    "url": "/ONLINE_STORE_FE/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "05884ccb67cdca0a266f",
+    "url": "/ONLINE_STORE_FE/static/css/main.ac200c67.chunk.css"
+  },
+  {
+    "revision": "d41e08de5520826bd24f",
+    "url": "/ONLINE_STORE_FE/static/js/2.dff255aa.chunk.js"
+  },
+  {
+    "revision": "05884ccb67cdca0a266f",
+    "url": "/ONLINE_STORE_FE/static/js/main.1c653628.chunk.js"
+  },
+  {
+    "revision": "84d39aa3827ee162c34c",
+    "url": "/ONLINE_STORE_FE/static/js/runtime-main.4a1a89fb.js"
+  },
+  {
+    "revision": "f58999fdce0868eda1cec36a14714d3a",
+    "url": "/ONLINE_STORE_FE/static/media/profile1.f58999fd.png"
+  },
+  {
+    "revision": "f8925e1770e252bd19cb17d14c8ad1fc",
+    "url": "/ONLINE_STORE_FE/static/media/profile2.f8925e17.png"
+  }
+]);
